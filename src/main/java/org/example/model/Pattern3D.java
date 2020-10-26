a0a35550bb701c5a65d99b7b250ee3e56193560d
Ben Dzaebel <bdzaebel@inovex.de>
2020-10-26 20:45:57 +0100

    clean up Pattern3D

--- src/main/java/org/example/model/Pattern3D.java
+++ src/main/java/org/example/model/Pattern3D.java
@@ -7,11 +7,6 @@ public class Pattern3D {
     private int size;
     private int[][][] values;
 
-    public Pattern3D(int size, int[][][] values) {
-        this.size = size;
-        this.values = values;
-    }
-
     public Pattern3D(int size) {
         this.size = size;
         this.values = new int[size][size][size];
@@ -40,80 +35,6 @@ public class Pattern3D {
         return values;
     }
 
-    /**
-     * Clockwise 90 degree rotation around the X axis
-     */
-    public Pattern3D getXRotated() {
-        int[][][] rotated = new int[size][size][size];
-        for (int x = 0; x < size; x++) {
-            for (int y = 0; y < size; y++) {
-                for (int z = 0; z < size; z++) {
-                    rotated[z][y][x] = get(x,size-z-1,y);
-                }
-            }
-        }
-        return new Pattern3D(size, rotated);
-    }
-
-    /**
-     * Clockwise 90 degree rotation around the Z axis
-     */
-    public Pattern3D getZRotated() {
-        int[][][] rotated = new int[size][size][size];
-        for (int x = 0; x < size; x++) {
-            for (int y = 0; y < size; y++) {
-                for (int z = 0; z < size; z++) {
-                    rotated[z][y][x] = get(y,size-x-1,z);
-                }
-            }
-        }
-        return new Pattern3D(size, rotated);
-    }
-
-    /**
-     * Clockwise 90 degree rotation around the y axis
-     */
-    public Pattern3D getYRotated() {
-        int[][][] rotated = new int[size][size][size];
-        for (int x = 0; x < size; x++) {
-            for (int y = 0; y < size; y++) {
-                for (int z = 0; z < size; z++) {
-                    rotated[z][y][x] = get(size-z-1,y,x);
-                }
-            }
-        }
-        return new Pattern3D(size, rotated);
-    }
-
-
-
-//    public int[] getEdge(Direction direction) {
-//        int[] edge = new int[size];
-//        switch (direction) {
-//            case LEFT:
-//                for (int y = 0; y < size; y++) {
-//                    edge[y] = get(0, y);
-//                }
-//                break;
-//            case RIGHT:
-//                for (int y = 0; y < size; y++) {
-//                    edge[y] = get(size - 1, y);
-//                }
-//                break;
-//            case UP:
-//                for (int x = 0; x < size; x++) {
-//                    edge[x] = get(x, 0);
-//                }
-//                break;
-//            case DOWN:
-//                for (int x = 0; x < size; x++) {
-//                    edge[x] = get(x, size - 1);
-//                }
-//                break;
-//        }
-//        return edge;
-//    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -135,10 +56,5 @@ public class Pattern3D {
         int result = Objects.hash(size);
         result = 31 * result + Arrays.hashCode(values);
         return result;
-    }
-
-    @Override
-    public String toString() {
-        return "TODO";
     }
 }
